11d08b012d7d2260356caa3ae14162699cae6754
ResArt <resarttm@mail.ru>
2025-11-30 13:24:56 +0400

    fix

--- my_site/myproject/urls.py
+++ my_site/myproject/urls.py
@@ -20,8 +20,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    url(r'^', include('main.urls')),
+    path('mysite-admin/', admin.site.urls),
+    #url(r'^', include('main.urls')),
     # ваши другие маршруты...
 ]
 
